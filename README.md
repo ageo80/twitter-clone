0733ca3701544224d14cef8522a27834725b26bf
Jeremy Miller <jeremy@dischan.org>
2014-07-15 23:51:03 -0300

    Work on user model.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Twitter Clone
 =============
 
-A simple clone of Twitter built as a CodeIgniter experiment. This project was created for learning purposes only, use in production is not recommended.
+A simple clone of Twitter built as a CodeIgniter experiment. This project was created for learning purposes only, therefore use in production is not recommended.
 
 Frameworks Used:
 PHP  - [CodeIgniter](http://ellislab.com/codeigniter)
--- application/models/user.php
+++ application/models/user.php
@@ -1,5 +1,5 @@
 <?php
-  class user extends CI_Model {
+  class User extends CI_Model {
 
     public function __construct(){
       $this->load->database();
@@ -13,6 +13,24 @@
 
       $query = $this->db->get_where('user', array('slug' => $slug));
       return $query->row_array();
+    }
+
+    public function set_user() {
+      $this->load->helper('url');
+      $this->load->helper('security');
+
+      $slug = url_title($this->input->post('username'), 'dash', TRUE);
+      $password = $this->input->post('password');
+      $passwordVersion = 1;
+
+      $data = array(
+        'username' => $this->input->post('username'),
+        'email' => $this->input->post('email'),
+        'slug' => $slug,
+        'text' => $this->input->post('text')
+      );
+
+      return $this->db->insert('news', $data);
     }
   }
 
