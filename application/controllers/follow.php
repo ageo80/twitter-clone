dd0a86233ff97b75a2ccdcb9788f7bf1dc1fa644
Jeremy Miller <jeremy@dischan.org>
2014-07-22 12:24:29 -0300

    Improved follow/unfollow behaviour a bit

--- application/controllers/follow.php
+++ application/controllers/follow.php
@@ -5,6 +5,7 @@
     public function __construct() {
       parent::__construct();
       $this->load->model('follow_model');
+      $this->load->model('user_model');
       $this->load->helper('url');
       $this->load->library('form_validation');
     }
@@ -12,12 +13,13 @@
     public function follow_user(){
       if($this->session->userdata('logged_in')){
         $this->form_validation->set_rules('target-id', 'Follow User', 'required|is_natural_no_zero');
+        $user = $this->user_model->get_user($this->input->post('target-id'));
         if ($this->form_validation->run() == FALSE) {
           //failed validation
         } else {
           //passed validation
           $this->follow_model->set_follow();
-          redirect('/', 'location');
+          redirect('/' . $user['slug'], 'location');
         }
       } else {
         //login error
@@ -27,12 +29,13 @@
     public function unfollow_user(){
       if($this->session->userdata('logged_in')){
         $this->form_validation->set_rules('target-id', 'Follow User', 'required|is_natural_no_zero');
+        $user = $this->user_model->get_user($this->input->post('target-id'));
         if ($this->form_validation->run() == FALSE) {
           //failed validation
         } else {
           //passed validation
           $this->follow_model->delete_follow();
-          redirect('/', 'location');
+          redirect('/' . $user['slug'], 'location');
         }
       } else {
         //login error
