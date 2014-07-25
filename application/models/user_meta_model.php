59784dd6df2bf1e082748f90c3930decedef6a68
Jeremy Miller <jeremy@dischan.org>
2014-07-24 21:43:33 -0300

    Bug Fix: Will no longer change custom avatar to default on profile save

--- application/models/user_meta_model.php
+++ application/models/user_meta_model.php
@@ -13,12 +13,21 @@
     }
 
     public function save_user_meta() {
-      $data = array(
-        'user_id' => $this->session->userdata('id'),
-        'website' => $this->input->post('website'),
-        'about' => $this->input->post('about'),
-        'avatar' => $this->input->post('avatar_filename')
-      );
+      if($this->input->post('avatar_filename')){
+        $data = array(
+          'user_id' => $this->session->userdata('id'),
+          'website' => $this->input->post('website'),
+          'about' => $this->input->post('about'),
+          'avatar' => $this->input->post('avatar_filename')
+        );
+      } else {
+        $data = array(
+          'user_id' => $this->session->userdata('id'),
+          'website' => $this->input->post('website'),
+          'about' => $this->input->post('about'),
+        );
+      }
+      
 
       if($user_meta_array = $this->get_user_meta_by_user_id($this->session->userdata('id'))){
         return $this->db->update('user_meta', $data, array('id' => $user_meta_array['id']));
